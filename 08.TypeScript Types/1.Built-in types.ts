90812db755e7bd209bbb0a77ff25162ebf531500
rajun <rajunpandey1234@gmail.com>
2024-09-11 12:58:52 +0545

    learned typescript type

---
+++ 08.TypeScript Types/1.Built-in types.ts
@@ -0,0 +1,69 @@
+/*
+//1.Number
+//example
+// Integer number
+let age: number = 30;
+// Float number
+let marks: number = 30.5;
+*/
+
+/*
+//2.string
+//example
+let first_name: string = 'rajun';
+let last_name: string = "pandey";
+let full_name: string = `${first_name} ${last_name}`;
+*/
+
+/*
+//3.Boolean
+// Boolean data type allows you to represent logical entities.
+// It stores the either true or false value.
+//example
+let isReady: boolean = true;
+*/
+
+/*
+//4.Symbol
+//The symbol is a primitive data type, which is mostly used to create unique values
+//Example
+// Define a symbol
+const UNIQUE_KEY = Symbol(); 
+// Use the symbol as a property key in an object
+let obj = {
+[UNIQUE_KEY]: "SecretValue"
+};
+*/
+
+/*
+//5.Null
+//The null type in TypeScript represents the intentional absence of any object value
+//Example
+let empty: null = null;
+*/
+
+/*
+//6.Undefined
+//The undefined data type represents the absence of value
+//Example
+let undef: undefined;
+*/
+
+/*
+//7.Object
+//The object is a non-primitive data type,
+// which can contain any value that is not a number, string, boolean, symbol, null, or undefined. 
+//Example
+let person: object = {name: "rajun"};
+*/
+
+/*
+//8.Void
+//The void type is used in the return type of functions that do not return a value.
+//Example
+function log(): void {
+    console.log("log");
+    }
+    */
+
+
---
+++ 08.TypeScript Types/2.User-defined Types.ts
@@ -0,0 +1,22 @@
+/*
+//1.Array
+//The array data type is a collection of the same elements.
+// It stores the elements, which you can access or update using the array index that starts from 0.
+//example
+let numbers: number[] = [1, 2, 3];
+*/
+
+/*
+//2.Tuple
+//Tuple types allow you to express an array with a fixed number of elements whose types are known, but need not be the same
+//example
+let tuple: [string, number] = ["hello", 10];
+console.log(tuple);
+*/
+
+//3.Enum
+//An enumeration is a collection of related values that can be numeric or string values.
+//example
+enum Color {Red, Green, Blue}
+let c: Color = Color.Green;
+console.log(c);
