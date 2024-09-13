67bac2adcb7598711105becd2931673df1a903ae
rajun <rajunpandey1234@gmail.com>
2024-09-13 12:11:30 +0545

    learned  Modules in ts

---
+++ 38. TypeScript Modules.ts/1. Internal Module.ts
@@ -0,0 +1,25 @@
+//A module is designed with the idea to organize code written in TypeScript.
+
+
+
+/*
+//internal module:used to logically group classes, interfaces, functions into one unit and can be exported in another module.
+
+*/
+
+/*
+//Internal Module Syntax (Old)
+module TutorialPoint { 
+   export function add(x, y) {  
+      console.log(x+y); 
+   } 
+}
+   */
+
+/*
+//Namespace Syntax (New)
+namespace TutorialPoint { 
+   export function add(x, y) { console.log(x + y);} 
+}
+   */
+
